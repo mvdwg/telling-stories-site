332ff837cbbdc6ac658d497c8474fb845d42d373
Santiago Ferreira <san650@gmail.com>
2017-02-28 18:26:13 -0300

    Generate visual documentation site

--- index.js
+++ index.js
@@ -2,5 +2,19 @@
 'use strict';
 
 module.exports = {
-  name: 'telling-stories-site'
+  name: 'telling-stories-site',
+
+  postBuild: function(result) {
+    if (!this.isEnabled) {
+      return;
+    }
+
+    var dir = result.directory;
+    var fs = require('fs');
+    var path = require('path');
+
+    fs.unlinkSync(path.resolve(dir, 'index.html'));
+    fs.renameSync(path.resolve(dir, 'tests/index.html'), path.resolve(dir, 'runner.html'));
+    fs.renameSync(path.resolve(dir, 'telling-stories-site/viewer.html'), path.resolve(dir, 'index.html'));
+  },
 };
